321c65917e8a988a824528ca4f3606b91f6ab503
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2023-11-08 20:50:53 +0100

    Register db for program CarWorkshop

--- CarWorkshop.MVC/Program.cs
+++ CarWorkshop.MVC/Program.cs
@@ -1,7 +1,10 @@
+using CarWorkshop.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CarWorkshopDbContext>();
 
 var app = builder.Build();
 
