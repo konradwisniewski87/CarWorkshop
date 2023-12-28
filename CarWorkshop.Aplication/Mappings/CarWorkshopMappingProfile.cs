1dc06383702dec61330c9913784a51c20b8f625d
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2023-12-28 18:52:24 +0100

    Data transfer object(DTO) added with AutoMapper extension

---
+++ CarWorkshop.Aplication/Mappings/CarWorkshopMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarWorkshop.Application.Mappings
+{
+    public class CarWorkshopMappingProfile : Profile
+    {
+        public CarWorkshopMappingProfile()
+        {
+            CreateMap<CarWorkshopDTO, Domain.Entities.CarWorkshop>()
+                .ForMember(e => e.ContactDetails, opt => opt.MapFrom(src => new CarWorkshopContactDetails()
+                {
+                    City = src.City,
+                    PhoneNumber = src.PhoneNumber,
+                    PostCode = src.PostCode,
+                    Street = src.Street,
+                }));
+                //if is the same name and type, automapper automaticly mapping value, but in this case we must mapping from type values to object
+        }
+    }
+}
