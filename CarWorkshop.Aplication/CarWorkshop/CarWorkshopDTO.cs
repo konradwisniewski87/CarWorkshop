4c9b3c6c766e072e0d42637041aaa78fb7b1b172
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2023-12-30 21:40:07 +0100

    Adding validation to the model

--- CarWorkshop.Aplication/CarWorkshop/CarWorkshopDTO.cs
+++ CarWorkshop.Aplication/CarWorkshop/CarWorkshopDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,19 @@ namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDTO
     {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string Name { get; set; } = default!;
+
+        [Required(ErrorMessage = "Pease insert description")]//force an error message my content
         public string? Description { get; set; }
         public string? About { get; set; }
+
+        [StringLength(15, MinimumLength = 6)]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
+
         public string? PostCode { get; set; }
 
         public string? EncodedName { get; set; }
--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -21,8 +21,13 @@ namespace CarWorkshop.MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CarWorkshopDTO carWorkshop)
         {
+            if(!ModelState.IsValid)
+            {
+                return View(carWorkshop);
+            }
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create));//TODO: refactor
+
         }
     }
 }
